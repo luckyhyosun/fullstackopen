5468601443385b6cc68a094fd679a46b71eb0e3a
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-06-10 21:46:46 +0200

    Finished the study materials

--- part1/a/introdemo/src/App.jsx
+++ part1/a/introdemo/src/App.jsx
@@ -1,15 +1,33 @@
+const Greeting = (props) => {
+  return <div>
+      <p>{props.name} from {props.city}</p>
+    </div>
+}
+
 const App = () => {
   console.log("I love React yayyyy");
-  const now = new Date();
-  const firstName = "hyosun";
-  const lastName = "kim";
-  console.log(now, firstName+" "+lastName)
+  // const firstName = "hyosun";
+  // const lastName = "kim";
+  // const city = "Stockholm";
+  const user01 = {
+    name: "hyosun",
+    city: "Stockholm"
+  };
+  const user02 = {
+    name: "kim",
+    city: "Seoul"
+  }
+  const status = ["student", "KTH", 1]
 
   return (
-    <div>
-      <h1>My name is ... {firstName} {lastName}</h1>
-      <p>Hello React world, it is {now.toString()}</p>
-    </div>
+    //<div>
+    <>
+      <h2>Hur mår du?</h2>
+      <Greeting name = {user01.name} city = {user01.city}/>
+      <Greeting name = {user02.name} city = {user02.city}/>
+      <p>She is a {status[2]}year {status[0]} of {status[1]}, </p>
+    </>
+    //</div>
   )
 }
 
