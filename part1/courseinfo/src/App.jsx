c063cd4a4d5846fae2d936720a43a7b9e4c18f0e
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-06-13 16:05:22 +0200

    Finished the study materials of part1/b & c

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,51 +1,24 @@
-const Header = (props) => {
-  return <h1>{props.course}</h1>
-}
-const Part = (props)=> {
-  return <p>{props.part} {props.exercises}</p>
-}
-const Content = (props) => {
-  return (
-    <div>
-      {/* <p>{props.part1} {props.exercises1}</p>
-      <p>{props.part2} {props.exercises2}</p>
-      <p>{props.part3} {props.exercises3}</p> */}
-      <Part part = {props.parts[0].name} exercises = {props.parts[0].exercises}/>
-      <Part part = {props.parts[1].name} exercises = {props.parts[1].exercises}/>
-      <Part part = {props.parts[2].name} exercises = {props.parts[2].exercises}/>
-    </div>
-  )
-}
-const Total = (props) => {
-  const num = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises;
-  return <p>📝The number of exercises {num}</p>
-}
+import { useState } from 'react'
+//destructuring
+const Display = ({counter}) => <p>{counter}</p>
+
+//destructuring
+const Button = ({onClick, name}) =>  <div><button onClick={onClick}>{name}</button></div>
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const [ counter, setCounter ] = useState(0)
+
+  function increase (){setCounter(counter + 1)};
+  function decrease (){setCounter(counter - 1)};
+  function zero (){setCounter(0)}
 
   return (
     <div>
-      <Header course = {course.name}/>
-      <h2>👉Remember: "The only way to go fast, is to go well"👈</h2>
-      <Content parts={course.parts}/>
-      <Total parts={course.parts}/>
+      <Button onClick={increase} name = "increase" />
+      <Display counter = {counter} />
+
+      <Button onClick={decrease} name = "decrease" />
+      <Button onClick={zero} name = "zero" />
     </div>
   )
 }
