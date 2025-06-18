2912d86563c5465337012aaf3a809fb1d52edfd4
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-06-18 23:09:09 +0200

    Start the Excercise Anecdotes

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+
+const App = () => {
+  const anecdotes = [
+    '0: If it hurts, do it more often.',
+    '1: Adding manpower to a late software project makes it later!',
+    '2: The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    '3: Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    '4: Premature optimization is the root of all evil.',
+    '5: Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    '6: Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    '7: The only way to go fast, is to go well.'
+  ]
+
+  const [selected, setSelected] = useState(0);
+  const [voteArray, setVoteArray] = useState([]);
+
+  const randomNum = Math.floor(Math.random()* (anecdotes.length));
+
+  const votes = new Array(8).fill(0);
+
+  const voteFunction = () => {
+    console.log(selected);
+    const newVoteArray = [...votes];
+    newVoteArray[selected] += 1;
+    setVoteArray(newVoteArray);
+    console.log(voteArray);
+
+  }
+
+  return (
+    <div>
+      <p>{anecdotes[selected]}</p>
+      <button onClick={voteFunction}>Vote</button>
+      <button onClick={() => {setSelected(randomNum)}}>Next Anecdote</button>
+    </div>
+  )
+}
+
+export default App
