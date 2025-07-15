ef602c98417f28614efa22ebb855114e293bf9cd
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-07-15 21:37:52 +0900

    Start the study mateirals of part3/b

--- part3/abcd/index.js
+++ part3/abcd/index.js
@@ -108,7 +108,7 @@ app.post('/api/notes', (req, res) => {
   response.json(note);
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
