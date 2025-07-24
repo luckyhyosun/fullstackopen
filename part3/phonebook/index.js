6cedfd9bf907b9d7fdd2d12ae7f2782abc96b79b
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-07-24 21:58:16 +0900

    Start the study materials of part3/c

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 // const morgan = require('morgan');
-const cors = require('cors');
+// const cors = require('cors');
 const app = express();
 
 app.use(express.static('dist'));
 app.use(express.json());
 // app.use(morgan(':method :url :status :response-time ms :reqBody'));
-app.use(cors());
+// app.use(cors());
 
 let persons = [
     {
