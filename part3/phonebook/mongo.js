1a133e06fe1af74bb65c5f28ca57140455ec1eb7
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-07-28 22:49:56 +0900

    Completed Phonebook Exercise 3.12.

---
+++ part3/phonebook/mongo.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const personName = process.argv[3]
+const personNumber = process.argv[4]
+
+// console.log(personName, personNumber);
+
+const url = `mongodb+srv://luckyFullstack:${password}@cluster0.dguebgx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const personScheme = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personScheme)
+
+const person = new Person({
+  name: personName,
+  number: personNumber,
+})
+
+// console.log(person);
+
+// person.save().then(result => {
+//   console.log(`added ${personName} number ${personNumber} to phonebook`)
+//   mongoose.connection.close()
+// })
+
+Person
+  .find({})
+  .then(persons=> {
+    persons.forEach(person => {
+      console.log(person);
+    })
+    mongoose.connection.close()
+  })
