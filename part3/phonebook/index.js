a086540c61af845745695eeba0ad704ec3b9f9aa
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-08-03 12:46:37 +0200

    Completed Phonebook Exercise 3.18.

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -5,30 +5,36 @@ const Person = require('./models/person');
 
 const app = express();
 
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
 app.use(express.static('dist'));
 app.use(express.json());
 
 app.get('/api/persons', (req, res) => {
-    Person.find({}).then((persons => {
-        res.json(persons);
-    }))
+    Person.find({}).then((persons => res.json(persons)))
 });
 
 app.get('/info', (req, res) => {
-    res.send(`
-        <p>Phonebook has info for 2 people</p>
+   Person.find({}).then(persons => {
+    return res.send(`
+        <p>Phonebook has info for ${persons.length} people</p>
         ${new Date()}
     `)
+   })
 });
 
 app.get('/api/persons/:id', (req, res) => {
     const id = req.params.id;
-    // const foundPerson = persons.find(person => person.id === id);
-    // if(foundPerson){res.json(foundPerson)}
-    // else{res.status(404).end()}
-    Person.findById(id).then((person) => {
-        res.json(person)
-    })
+
+    Person.findById(id).then(person => res.json(person))
 });
 
 app.post('/api/persons', (req, res) => {
@@ -38,24 +44,23 @@ app.post('/api/persons', (req, res) => {
         return res.status(400).json({error: 'Bad Request: content missing'})
     }
 
-    // const checkName = persons.find(p => p.name === body.name);
+    Person.find({}).then(persons => {
+        persons.forEach(person => {
+            if(person.name === body.name){
+                const id = body.id
+                return app.put(`/api/persons/:${id}`, (req, res) => {
+                    person.number = body.number
 
-    // if(checkName){
-    //     return res.status(409).json({error: 'name must be unique'});
-    // }
+                    return person.save().then(updatedPerson => res.json(updatedPerson))
+                })
+            }
+        })
+    })
 
     const newPerson = new Person({
       "name": body.name,
       "number": body.number
     })
-
-    // console.log(JSON.stringify(req.body));
-    // morgan.token('body', JSON.stringify(req.body));
-    // morgan.token('reqBody', function (req, res) { return JSON.stringify(req.body) })
-
-    // persons = persons.concat(newPerson);
-    // return res.status(200).json(newPerson);
-
     newPerson.save().then((savedPerson) => {
         res.json(savedPerson)
     })
@@ -63,10 +68,12 @@ app.post('/api/persons', (req, res) => {
 
 app.delete('/api/persons/:id', (req, res) => {
     const id = req.params.id;
-    persons = persons.filter(p => p.id !== id);
-
-    res.status(204).end();
+    Person.findByIdAndDelete(id)
+        .then(result => res.status(204).end())
+        .catch(error => next(error))
 });
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
