a1f2cfc4c41de7f1d8147e3f6acef350cb90d3ab
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-08-12 15:41:29 +0200

    Setting Web Server with Express

--- part3/mypractice/backend/index.js
+++ part3/mypractice/backend/index.js
@@ -1,9 +1,55 @@
-console.log('hello world')
-const http = require('http')
+const express = require('express')
+const app = express()
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('Hello World')
+const animals = [
+    {
+      "id": "1",
+      "name": "African Elephant",
+      "classification": "Mammal",
+      "habitat": "Savannas, forests, and grasslands of Sub-Saharan Africa",
+      "importantNote": "Largest land animal on Earth; highly intelligent and social.",
+      "endangered": false
+    },
+    {
+      "id": "2",
+      "name": "Bald Eagle",
+      "classification": "Bird",
+      "habitat": "Near lakes, rivers, and coastal regions in North America",
+      "importantNote": "Symbol of the United States; known for its excellent eyesight and powerful flight.",
+      "endangered": false
+    },
+    {
+      "id": "3",
+      "name": "Green Sea Turtle",
+      "classification": "Reptile",
+      "habitat": "Tropical and subtropical oceans, often near coral reefs",
+      "importantNote": "Long-distance migrator; endangered due to plastic pollution and poaching.",
+      "endangered": true
+    },
+    {
+      "id": "4",
+      "name": "Arctic Fox",
+      "classification": "Mammal",
+      "habitat": "Arctic tundra regions of the Northern Hemisphere",
+      "importantNote": "Has thick fur that changes color with the seasons (white in winter, brown in summer) for camouflage.",
+      "endangered": true
+    },
+    {
+      "id": "5",
+      "name": "Poison Dart Frog",
+      "classification": "Amphibian",
+      "habitat": "Tropical rainforests of Central and South America",
+      "importantNote": "Brightly colored as a warning; some species have skin toxins strong enough to deter predators or harm humans.",
+      "endangered": true
+    }
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Animal book!</h1>')
+})
+
+app.get('/api/animals', (request, response) => {
+  response.json(animals)
 })
 
 const PORT = 3001
