210e0d107a4c3724a3fe8b64f60db432eac3a856
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-08-13 13:28:09 +0300

    Depolying backend with Fly.io

--- part3/mypractice/frontend/src/services/animals.js
+++ part3/mypractice/frontend/src/services/animals.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/animals'
+const baseUrl = 'http://localhost:3001/api/animals'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
