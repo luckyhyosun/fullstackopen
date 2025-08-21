c6bd6a1d7d70f348849d31a92f9a6b4ae67c32d5
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-08-21 15:22:26 +0200

    Add Model: blogModel.js

--- part4/blogList/controllers/blogRouters.js
+++ part4/blogList/controllers/blogRouters.js
@@ -1,22 +1,5 @@
 const blogRouter = require('express').Router()
-const mongoose = require('mongoose')
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-blogSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = require('../models/blogModel')
 
 blogRouter.get('/', (request, response) => {
   Blog.find({}).then((blogs) => {
@@ -25,7 +8,14 @@ blogRouter.get('/', (request, response) => {
 })
 
 blogRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body)
+  const body = request.body
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  })
 
   blog.save().then((result) => {
     response.status(201).json(result)
