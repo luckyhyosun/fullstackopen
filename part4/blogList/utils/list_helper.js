da7724fdba1acd9a03b6da5ca5006d50ddc03964
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-08-22 19:34:31 +0200

    Completed the helper function exercise 4.4.

--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -1,7 +1,19 @@
 const dummy = (blogs) => {
-  // ...
+  if(blogs){
+    return 1
+  }
+}
+
+const totalLikes = (blog) => {
+  if(blog.length === 0){
+    return 0
+  }
+
+  const result = blog[0].likes
+  return result
 }
 
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
