15fd145a812cb4464147d9a3188d4db22886168d
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-09-05 16:28:17 +0200

    Creating a note with userId

--- part4/note_backend/controllers/notes.js
+++ part4/note_backend/controllers/notes.js
@@ -1,5 +1,6 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
 notesRouter.get('/', async (req, res) => {
   const notes = await Note.find({})
@@ -19,6 +20,11 @@ notesRouter.get('/:id', async (req, res) => {
 
 notesRouter.post('/', async (req, res) => {
   const body = req.body
+  const user = await User.findById(body.userId)
+
+  if(!user){
+    return res.status(400).json({ error: 'userId missing or not valid' })
+  }
 
   if (!body.content) {
     return res.status(400).json({ error: 'Bad Request: content missing' })
@@ -27,9 +33,13 @@ notesRouter.post('/', async (req, res) => {
   const note = new Note({
     content: body.content,
     important: body.important || false,
+    user: user._id
   })
 
   const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
+
   res.status(201).json(savedNote)
 })
 
