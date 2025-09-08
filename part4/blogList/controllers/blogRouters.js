577713b1f439886dca3879bd921d0f43d5b1c739
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-09-08 13:42:56 +0200

    Blog list expansion - each blog contains user id

--- part4/blogList/controllers/blogRouters.js
+++ part4/blogList/controllers/blogRouters.js
@@ -1,5 +1,6 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blogModel')
+const User = require('../models/userModel')
 
 blogRouter.get('/', async (request, response) => {
   //Returns an array of Mongoose documents, not Promise ojbect.
@@ -10,6 +11,12 @@ blogRouter.get('/', async (request, response) => {
 
 blogRouter.post('/', async (request, response) => {
   const body = request.body
+
+  const user = await User.findById(body.userId)
+
+  if(!user){
+    return response.status(400).json({ error: 'userId missing or not valid'})
+  }
 
   if (!body.title || !body.url) {
     return response
@@ -22,6 +29,7 @@ blogRouter.post('/', async (request, response) => {
     author: body.author,
     url: body.url,
     likes: body.likes,
+    user: user._id
   })
 
   const savedBlog = await blog.save()
