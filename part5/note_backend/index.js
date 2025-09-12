754d7f36bba96c886a4897a1716c107cf3a0dc8b
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-09-12 21:36:24 +0200

    Add middleware functions

--- part5/note_backend/index.js
+++ part5/note_backend/index.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.use(express.json())
-
 let notes = [
     {
         "id": "1",
@@ -20,6 +18,17 @@ let notes = [
         "important": true
     }
 ]
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(express.json())
+app.use(requestLogger)
 
 app.get('/', (request, response) => {
     response.json('<h1>Hello</h1>')
@@ -65,6 +74,12 @@ app.delete('/api/notes/:id', (request, response) => {
 
     response.status(204).end()
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT)
