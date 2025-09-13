a2fe52fbc0de736ced455d03a82e4e92b4fa5e33
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-09-14 01:13:01 +0200

    Fix typo

--- part5/note_backend/index.js
+++ part5/note_backend/index.js
@@ -5,5 +5,5 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 
 app.listen(config.PORT, () =>
-  logger.info(`Server running on port ${process.env.PORT}`),
+  logger.info(`Server running on port ${config.PORT}`)
 )
