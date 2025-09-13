b66686372f823cb2d04803d291f3f93cce8192f6
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-09-13 23:45:55 +0200

    Separate the different responsibilities into separate modules - app.js/ index.js

---
+++ part5/note_backend/app.js
@@ -0,0 +1,30 @@
+// defining the application
+// the actual Express application
+// establishing the connection to the database
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const notesRouter = require('./controllers/notes')
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.set('strictQuery', false)
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => logger.info('connected to MongoDB'))
+  .catch((error) => logger.error('error connecting to MongoDB:', error.message))
+
+const app = express()
+
+app.use(express.static('dist'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/notes', notesRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
--- part5/note_backend/index.js
+++ part5/note_backend/index.js
@@ -1,21 +1,8 @@
-const express = require('express')
-
+// imports the actual application from the app.js file
+// and then starts the application
+const app = require('./app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const middleware = require('./utils/middleware')
-
-const notesRouter = require('./controllers/notes')
-
-const app = express()
-
-app.use(express.static('dist'))
-app.use(express.json())
-app.use(middleware.requestLogger)
-
-app.use('/api/notes', notesRouter)
-
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
 
 app.listen(config.PORT, () =>
   logger.info(`Server running on port ${process.env.PORT}`),
