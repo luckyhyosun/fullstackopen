55752d3bd2936855a9ec87b83dceccd930ec2b4c
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-09-13 19:41:22 +0200

    Validation error handling

--- part5/note_backend/index.js
+++ part5/note_backend/index.js
@@ -36,7 +36,7 @@ app.get('/api/notes/:id', (request, response) => {
         .catch(error => next(error))
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
 
     if(!body.content){
@@ -50,7 +50,9 @@ app.post('/api/notes', (request, response) => {
         important: body.important || false
     })
 
-    newNote.save().then(newNote => response.json(newNote))
+    newNote.save()
+        .then(newNote => response.json(newNote))
+        .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
@@ -88,6 +90,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  }else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
--- part5/note_backend/models/note.js
+++ part5/note_backend/models/note.js
@@ -9,7 +9,11 @@ mongoose.connect(url)
     .catch(error => console.log('error connecting to MongoDB:', error.message))
 
 const noteSchema = new mongoose.Schema({
-  content: String,
+  content: {
+    type: String,
+    minLength: 5,
+    required: true
+  },
   important: Boolean,
 })
 
