3957eefece29d7091989f6fa67359bea05064f7b
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-09-14 23:01:20 +0200

    Create a new note with authentification (token)

--- part5/note_frontend/src/App.jsx
+++ part5/note_frontend/src/App.jsx
@@ -79,6 +79,7 @@ const App = () => {
     event.preventDefault()
     try {
       const user = await loginService.login({ username, password })
+      noteService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
--- part5/note_frontend/src/services/notes.js
+++ part5/note_frontend/src/services/notes.js
@@ -1,16 +1,22 @@
 import axios from 'axios'
 const baseUrl = '/api/notes'
 
+let token = null
+
+const setToken = newToken => {
+  token = `Bearer ${newToken}`
+}
+
 const getAll = () => {
     return axios
       .get(baseUrl)
       .then(response => response.data)
 }
 
-const create = newObj => {
-    return axios
-      .post(baseUrl, newObj)
-      .then(response => response.data)
+const create = async newObj => {
+    const config = {headers: { Authorization: token }}
+    const response =  await axios.post(baseUrl, newObj, config)
+    return response.data
 }
 
 const update = (id, updatedObj) => {
@@ -29,5 +35,6 @@ export default {
     getAll,
     create,
     update,
-    remove
+    remove,
+    setToken
 }
