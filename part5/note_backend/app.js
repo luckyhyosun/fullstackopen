ecdc0f4776470f0d20e45ae5fd8c0819a969ae07
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-09-14 13:52:43 +0200

    Create login router & Generate token using jwt

--- part5/note_backend/app.js
+++ part5/note_backend/app.js
@@ -8,6 +8,7 @@ const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const notesRouter = require('./controllers/notes')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -25,6 +26,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/notes', notesRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
