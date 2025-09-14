6392acfb8bf62e99cfe0ce182892bc5139271139
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-09-14 11:02:41 +0200

    Add get router to User

--- part5/note_backend/controllers/users.js
+++ part5/note_backend/controllers/users.js
@@ -3,6 +3,11 @@ const bcrypt = require('bcrypt')
 const userRouter = require('express').Router()
 const User = require('../models/user')
 
+userRouter.get('/', async (request, response) => {
+  const users = await User.find({})
+  response.status(200).json(users)
+})
+
 userRouter.post('/', async (request, response) => {
   const { id, name, pw } = request.body
 
