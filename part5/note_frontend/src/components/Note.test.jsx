8c4f0a279c9b659167c24b73673ac507d67f4213
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-09-17 23:58:30 +0200

    Test - Togglable component

--- part5/note_frontend/src/components/Note.test.jsx
+++ part5/note_frontend/src/components/Note.test.jsx
@@ -1,20 +1,61 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Note from './Note'
+import Togglable from './Togglable'
 
-test('renders content', async () => {
-  const note = {
-    content: 'Component testing is done with react-testing-library',
-    important: true
-  }
+describe('<Togglable />', () => {
+  beforeEach(() => {
+    render(
+      <Togglable buttonLabel="show...">
+        <div>togglable content</div>
+      </Togglable>
+    )
+  })
 
-  const mockHandler = vi.fn()
+  test('renders content', async () => {
+    const note = {
+      content: 'Component testing is done with react-testing-library',
+      important: true
+    }
 
-  render(<Note note={note} handelChangeImportant={mockHandler}/>)
+    const mockHandler = vi.fn()
 
-  const user = userEvent.setup()
-  const button = screen.getByText('🔁 Not important')
-  await user.click(button)
+    render(<Note note={note} handelChangeImportant={mockHandler}/>)
 
-  expect(mockHandler.mock.calls).toHaveLength(1)
+    const user = userEvent.setup()
+    const button = screen.getByText('🔁 Not important')
+    await user.click(button)
+
+    expect(mockHandler.mock.calls).toHaveLength(1)
+  })
+
+  test('renders its children', () => {
+    screen.getByText('togglable content')
+  })
+
+  test('at start the children are not displayed', () => {
+    const element = screen.getByText('togglable content')
+    expect(element).not.toBeVisible()
+  })
+
+  test('after clicking the button, children are displayed', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('show...')
+    await user.click(button)
+
+    const element = screen.getByText('togglable content')
+    expect(element).toBeVisible()
+  })
+
+  test('toggled content can be closed', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('show...')
+    await user.click(button)
+
+    const closeButton = screen.getByText('Cancel')
+    await user.click(closeButton)
+
+    const element = screen.getByText('togglable content')
+    expect(element).not.toBeVisible()
+  })
 })
