60e4cdafc809be6759a76100b5134bcbaf540e7c
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-09-18 23:37:00 +0200

    Playwright - writing on the form 1

--- part5/playwright/tests/note_app.spec.js
+++ part5/playwright/tests/note_app.spec.js
@@ -8,4 +8,15 @@ describe('Note app', () => {
     await expect(locator).toBeVisible()
     await expect(page.getByText('Note app by Lucky, Media Technology, Computer Science, KTH 2025')).toBeVisible()
   })
+
+  test('user can log in', async ({ page }) => {
+    await page.goto('http://localhost:5173')
+
+    await page.getByRole('button', { name: 'Login' }).click()
+    await page.getByRole('textbox').first().fill('developer')
+    await page.getByRole('textbox').last().fill('good')
+    await page.getByRole('button', { name: 'login' }).click()
+
+    await expect(page.getByText('Hello, hyosun! 👋')).toBeVisible()
+  })
 })
