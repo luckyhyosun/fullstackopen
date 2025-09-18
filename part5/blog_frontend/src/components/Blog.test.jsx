e876d550522cd5640f35abf1ee695353792b469b
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-09-18 10:34:23 +0200

    Blog Test - Togglable component

--- part5/blog_frontend/src/components/Blog.test.jsx
+++ part5/blog_frontend/src/components/Blog.test.jsx
@@ -1,24 +1,63 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
+import Togglable from './Togglable'
+import BlogDetail from './BlogDetail'
 
-test('<Blog /> component rendering with <BlogDetail /> comp', () => {
-  const blogDetail = {
-    title: 'Test title for Blog',
-    author: 'Kim developer',
-    likes: 0,
-    url: 'hello.com',
-    user: 'id1024'
-  }
+describe('<Blog /> component rendering', () => {
+  beforeEach(() => {
+    const blogDetail = {
+      title: 'Test title for Blog',
+      author: 'Kim developer',
+      likes: 0,
+      url: 'hello.com',
+      user: 'id1024'
+    }
 
-  render(
-    <div>
-      <Blog blog={blogDetail} />
-    </div>
-  )
+    render(
+      <div>
+        <Blog blog={blogDetail} />
+      </div>
+    )
+  })
 
-  const title = screen.getByText('Test title for Blog')
-  const author = screen.getByText(/Kim developer/, { exact: false })
+  test('swho blog detail', () => {
+    const title = screen.getByText('Test title for Blog')
+    const author = screen.getByText(/Kim developer/, { exact: false })
 
-  expect(title).toBeDefined()
-  expect(author).toBeInTheDocument()
+    expect(title).toBeDefined()
+    expect(author).toBeInTheDocument()
+  })
+})
+
+describe('<Togglable /> component renering', () => {
+  beforeEach(() => {
+    render(
+      <Togglable buttonLabel="More...">
+      </Togglable>
+    )
+  })
+
+  test('after clicking the button, children are displayed', async () => {
+    const blogDetail = {
+      title: 'Test title for Blog',
+      author: 'Kim developer',
+      likes: 0,
+      url: 'hello.com',
+      user: 'id1024'
+    }
+
+    const mockHandler = vi.fn()
+
+    render(<BlogDetail blog={blogDetail} handleToggle={mockHandler}/>)
+
+    const user = userEvent.setup()
+    const button = screen.getByText('More...')
+
+    await user.click(button)
+    const likes = screen.getByText('likes: 0')
+    const url = screen.getByText(/hello.com/, { exact : false })
+    expect(likes).toBeVisible()
+    expect(url).toBeVisible()
+  })
 })
