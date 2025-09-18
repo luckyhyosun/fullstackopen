b0045e61e77cc0fa117bafa7291bb3615042422e
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-09-18 23:50:11 +0200

    Playwright - test initialization

--- part5/playwright/tests/note_app.spec.js
+++ part5/playwright/tests/note_app.spec.js
@@ -1,17 +1,17 @@
-const { test, describe, expect } = require('@playwright/test')
+const { test, describe, expect, beforeEach } = require('@playwright/test')
 
 describe('Note app', () => {
-  test('front page can be opened', async ({ page }) => {
+  beforeEach(async ({ page }) => {
     await page.goto('http://localhost:5173')
+  })
 
+  test('front page can be opened', async ({ page }) => {
     const locator = page.getByText('Notes')
     await expect(locator).toBeVisible()
     await expect(page.getByText('Note app by Lucky, Media Technology, Computer Science, KTH 2025')).toBeVisible()
   })
 
   test('user can log in', async ({ page }) => {
-    await page.goto('http://localhost:5173')
-
     await page.getByRole('button', { name: 'Login' }).click()
     await page.getByLabel('Username:').fill('developer')
     await page.getByLabel('Password:').fill('good')
