29f5d004c602112635e6d2cdb3c89e59b2ace1bc
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-09-19 22:21:56 +0200

    Redux note initial code

--- part6/redux-note/src/main.jsx
+++ part6/redux-note/src/main.jsx
@@ -3,42 +3,58 @@ import ReactDOM from 'react-dom/client'
 
 import { createStore } from 'redux'
 
-const counterReducer = (state = 0, action) => {
+const noteReducer = (state = [], action) => {
   switch (action.type) {
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
-    case 'ZERO':
-      return 0
+    case 'NEW_NOTE' :
+      return state.concat(action.payload)
+    case 'TOGGLE_IMPORTANCE' :
+      return state.map(note =>
+        note.id === action.payload.id
+        ? {...note, important: !action.payload.important}
+        : note
+      )
     default:
       return state
   }
 }
 
-const store = createStore(counterReducer)
+const store = createStore(noteReducer)
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  payload: {
+    content: 'the app state is in redux store',
+    important: true,
+    id: 1
+  }
+})
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  payload: {
+    content: 'state changes are made with actions',
+    important: false,
+    id: 2
+  }
+})
+
+store.dispatch({
+  type: 'TOGGLE_IMPORTANCE',
+  payload: {
+    id: 2
+  }
+})
 
 const App = () => {
-  return (
+  return(
     <div>
-      <div>
-        {store.getState()}
-      </div>
-      <button
-        onClick={e => store.dispatch({ type: 'INCREMENT' })}
-      >
-        plus
-      </button>
-      <button
-        onClick={e => store.dispatch({ type: 'DECREMENT' })}
-      >
-        minus
-      </button>
-      <button
-        onClick={e => store.dispatch({ type: 'ZERO' })}
-      >
-        zero
-      </button>
+      <ul>
+        {store.getState().map(note=>
+          <li key={note.id}>
+            {note.content} <strong>{note.important ? 'important' : ''}</strong>
+          </li>
+        )}
+        </ul>
     </div>
   )
 }
