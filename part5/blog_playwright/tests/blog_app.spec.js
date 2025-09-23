6b599602c265dfb88feb2c80efb8d03bb4818f70
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-09-23 17:00:01 +0800

    Blog - succeed or fail login

--- part5/blog_playwright/tests/blog_app.spec.js
+++ part5/blog_playwright/tests/blog_app.spec.js
@@ -18,4 +18,20 @@ describe('Blog app', () => {
     const locator = page.getByText('Login Form')
     await expect(locator).toBeVisible()
   })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await page.getByLabel('Username:').fill('kim')
+      await page.getByLabel('Password:').fill('good')
+      await page.getByRole('button', {name: 'Login'}).click()
+      await expect(page.getByText('Hello, fullstack! 👋')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.getByLabel('Username:').fill('kim')
+      await page.getByLabel('Password:').fill('bad')
+      await page.getByRole('button', {name: 'Login'}).click()
+      await expect(page.getByText('not valid username or password')).toBeVisible()
+    })
+  })
 })
