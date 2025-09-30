28a33a65da8345c035230eaf4852743398723ecc
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-09-30 18:22:27 +0200

    Counter - defining the counter context in a separate file

--- part6/hook_counter/src/main.jsx
+++ part6/hook_counter/src/main.jsx
@@ -1,6 +1,10 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App'
+
+import { CounterContextProvider } from './CounterContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <CounterContextProvider>
+    <App />
+  </CounterContextProvider>
 )
