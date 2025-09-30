651f6d8bc6b65209d7ac91d31dbda947a1f1da50
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-09-30 17:17:49 +0200

    Anecdote Q - add createAnecdote using useMutation and validataion

--- part6/query_anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query_anecdotes/src/components/AnecdoteForm.jsx
@@ -1,10 +1,23 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { createAnecdote } from '../requests'
+
 const AnecdoteForm = () => {
+  const queryClient = useQueryClient()
+
+  const newAnecMutation = useMutation({
+    mutationFn: createAnecdote,
+    onSuccess: (response) => {
+      const anecdotes = queryClient.getQueryData(['anecdotes'])
+      queryClient.setQueryData(['anecdotes']), anecdotes.concat(response)
+    }
+  })
 
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    console.log('new anecdote')
+    if(content.length > 4){newAnecMutation.mutate({content})}
+    else{ window.alert("the text should be more than 5 words")}
 }
 
   return (
