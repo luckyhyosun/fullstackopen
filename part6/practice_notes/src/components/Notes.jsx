30a143280b7d21a4cdad191668ca88cfee046369
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-10-01 15:26:10 +0200

    Practice - separate functionalities into components

---
+++ part6/practice_notes/src/components/Notes.jsx
@@ -0,0 +1,24 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { toggleImportanceOf} from '../reducers/noteReducer'
+
+const Notes = () => {
+  const dispatch = useDispatch()
+  const notes = useSelector(state => state)
+
+  const toggleImportance = id => {
+    dispatch(toggleImportanceOf(id))
+  }
+
+  return <ul>
+  {notes.map(note=>
+    <li key={note.id}>
+      {note.content}
+      <button onClick={() => toggleImportance(note.id)}>
+        {note.important ? 'important' : 'Not important'}
+      </button>
+    </li>
+  )}
+  </ul>
+}
+
+export default Notes
