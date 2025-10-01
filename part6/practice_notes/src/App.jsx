1b2c392cec7cd8515ff799667d568f50ed088555
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-10-01 14:48:13 +0200

    Practice - action creators

--- part6/practice_notes/src/App.jsx
+++ part6/practice_notes/src/App.jsx
@@ -42,13 +42,45 @@ store.dispatch({
   }
 })
 
+const generateId = () =>
+  Number((Math.random() * 1000000).toFixed(0))
+
 const App = () => {
+  const addNote = event => {
+    event.preventDefault()
+    const content = event.target.note.value
+    event.target.note.value = ''
+
+    store.dispatch({
+      type: 'NEW_NOTE',
+      payload: {
+        content,
+        important: false,
+        id: generateId()
+      }
+    })
+  }
+
+  const toggleImportance = id => {
+    store.dispatch({
+      type: 'TOGGLE_IMPORTANCE',
+      payload: { id }
+    })
+  }
+
   return(
     <div>
+      <form onSubmit={addNote}>
+        <input name="note" />
+        <button type="submit">add</button>
+      </form>
       <ul>
         {store.getState().map(note=>
           <li key={note.id}>
-            {note.content} <button>{note.important ? 'important' : 'Not important'}</button>
+            {note.content}
+            <button onClick={() => toggleImportance(note.id)}>
+              {note.important ? 'important' : 'Not important'}
+            </button>
           </li>
         )}
         </ul>
