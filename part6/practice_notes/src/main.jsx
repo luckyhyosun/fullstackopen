efa9ff8309547c329789f27260dd7979d56f70c8
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-10-01 16:34:17 +0200

    Practice - install redux-toolkit and configureStore

--- part6/practice_notes/src/main.jsx
+++ part6/practice_notes/src/main.jsx
@@ -2,16 +2,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 
 import { Provider } from 'react-redux'
-import { createStore, combineReducers } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
+
 import noteReducer from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer'
 
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer
+const store = configureStore({
+  reducer: {
+    notes: noteReducer,
+    filter: filterReducer
+  }
 })
-
-const store = createStore(reducer)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
