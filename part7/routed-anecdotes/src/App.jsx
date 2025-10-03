dac914489eee7fcd9157487ccec886a720a10fec
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-10-03 23:55:54 +0200

    Anecdotes - add React Router to the application

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -1,15 +1,27 @@
 import { useState } from 'react'
 
-const Menu = () => {
+import {
+  BrowserRouter as Router,
+  Routes, Route, Link
+} from 'react-router-dom'
+
+const Menu = ({ anecdotes }) => {
   const padding = {
     paddingRight: 5
   }
   return (
-    <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
-    </div>
+    <Router>
+      <div>
+        <Link style={padding} to='/'>anecdotes</Link>
+        <Link style={padding} to='/create'>create new</Link>
+        <Link style={padding} to='/about'>about</Link>
+      </div>
+      <Routes>
+        <Route path="/create" element={<CreateNew />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/" element={<AnecdoteList anecdotes={anecdotes}/>} />
+      </Routes>
+    </Router>
   )
 }
 
@@ -125,10 +137,7 @@ const App = () => {
   return (
     <div>
       <h1>Software anecdotes</h1>
-      <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
+      <Menu anecdotes={anecdotes}/>
       <Footer />
     </div>
   )
