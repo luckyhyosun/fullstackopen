37ca5d9b5190711497a1065563573c59fbfa8225
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-10-11 23:51:04 +0200

    RRR Note - separate store function

--- part7/routed-blog-redux/src/main.jsx
+++ part7/routed-blog-redux/src/main.jsx
@@ -1,18 +1,8 @@
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom"
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import noteReducer from './reducers/noteReducer'
-import noteService from './services/notes'
+import store from './store'
 import App from './App'
-
-const store = createStore(noteReducer)
-
-noteService.getAll().then(notes => notes.forEach(note => {
-  store.dispatch({type: 'ADD_NOTE', payload: note.content})
-
-  console.log(store.getState())
-}))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
---
+++ part7/routed-blog-redux/src/store.js
@@ -0,0 +1,15 @@
+import { configureStore } from '@reduxjs/toolkit'
+import noteReducer from './reducers/noteReducer'
+import noteService from './services/notes'
+
+const store = configureStore({
+  reducer: noteReducer
+})
+
+noteService.getAll().then(notes => notes.forEach(note => {
+  store.dispatch({type: 'ADD_NOTE', payload: note.content})
+
+  console.log(store.getState())
+}))
+
+export default store
