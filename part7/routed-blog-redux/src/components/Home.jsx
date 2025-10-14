66cf65f479f59020d9b43d882ccafb86fc8d2645
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-10-15 00:41:11 +0200

    RRR Note - change db and show them in main page

--- part7/routed-blog-redux/src/components/Home.jsx
+++ part7/routed-blog-redux/src/components/Home.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from "react-router-dom"
 import { addNote } from '../reducers/noteReducer'
 import { createNotification } from '../reducers/notificationReducer'
@@ -10,6 +10,8 @@ const Home = () => {
   const [newNote, setNewNote] = useState('');
   const dispatch = useDispatch()
   const navigate = useNavigate()
+
+  const isUserLoggedIn = useSelector(state => state.users.loggedInUser)
 
   const addNewNotefnc = () => {
     console.log(newNote);
@@ -26,9 +28,18 @@ const Home = () => {
     <h2>React-Redux Note app</h2>
     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 
-    <h2>Add new Note</h2>
-    <input type="text" value={newNote} onChange={(e) => {setNewNote(e.target.value)}}/>
-    <button onClick={addNewNotefnc}>submit</button>
+    {!isUserLoggedIn
+      ? <h2>❗️ Please Login to Add a note 👋</h2>
+      : (
+        <div>
+          <h2>Add new Note</h2>
+          <input type="text" value={newNote} onChange={(e) => {setNewNote(e.target.value)}}/>
+          <button onClick={addNewNotefnc}>submit</button>
+        </div>
+      )
+    }
+
+
 
     <Users />
   </div>
--- part7/routed-blog-redux/src/components/Users.jsx
+++ part7/routed-blog-redux/src/components/Users.jsx
@@ -1,15 +1,30 @@
 import { useSelector } from 'react-redux'
 
 const Users = () => {
+  const border = {
+    border: "1px solid black",
+    padding: "10px"
+  }
+
   const allUsers = useSelector(state => state.users.all)
 
   return (<div>
     <h2>All Users</h2>
-    <ul>
-      {allUsers
-      ? allUsers.map(user => (<li key={user.id}>{user.username}</li>))
+    <table>
+      <tbody>
+          <tr>
+            <th style={border}>Name</th>
+            <th style={border}>Blogs</th>
+          </tr>
+        {allUsers
+      ? allUsers.map(user => (
+      <tr key={user.id}>
+        <td style={border}>{user.username}</td>
+        <td style={border}>{user.blogs.length}</td>
+      </tr>))
       : null}
-    </ul>
+      </tbody>
+    </table>
   </div>)
 }
 
