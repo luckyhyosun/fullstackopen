e9c5393ee84dd4993311365ea94fdab633563e87
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-10-21 17:13:27 +0200

    Connect mongoDB using mongoose and run server on a port using express

---
+++ part7/pracitce_blogs/server/app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+
+const app = express()
+
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => console.log('connected to MongoDB'))
+  .catch(error => console.log('error connecting to MongoDB:', error.message))
+
+module.exports = app
