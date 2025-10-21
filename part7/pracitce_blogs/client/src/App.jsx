995dd8de2aac1647dbbba675c843065d909f12d5
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-10-21 17:44:28 +0200

    FE - add components and use react-router-dom

--- part7/pracitce_blogs/client/src/App.jsx
+++ part7/pracitce_blogs/client/src/App.jsx
@@ -1,8 +1,29 @@
+import Home from './components/Home'
+import Blogs from './components/Blogs'
+import Login from './components/Login'
+
+import {
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom"
+
 function App() {
+  const padding = {
+    padding: 5
+  }
 
   return (
     <>
-      <h1>Client side</h1>
+      <Link style={padding} to="/">home</Link>
+      <Link style={padding} to="/blogs">blogs</Link>
+      <Link style={padding} to="/login">login</Link>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
   )
 }
