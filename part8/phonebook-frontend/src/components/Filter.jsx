17fa4e4a7e92cbcad4a6820ce424212c037e4007
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-10-26 16:01:05 +0100

    Add filter function

--- part8/phonebook-frontend/src/components/Filter.jsx
+++ part8/phonebook-frontend/src/components/Filter.jsx
@@ -1,3 +1,6 @@
+import { useState } from 'react'
+import { useSelector } from 'react-redux'
+
 const Filter = () => {
   const filterStyle = {
     backgroundColor: "#7FBAD7",
@@ -6,13 +9,23 @@ const Filter = () => {
     padding: "10px"
   }
 
+  const [filter, setFilter] = useState('')
+  const allUsers = useSelector(state => state.users)
+  const filteredUsers = allUsers.filter(user => user.username.toLowerCase().includes(filter.toLowerCase()))
+
   return (
     <div style={filterStyle}>
       <h2>Filter User</h2>
       <label htmlFor="filter">Filter shown with </label>
-      <input type="text" id="filter" />
+      <input type="text" id="filter" value={filter} onChange={({target}) => setFilter(target.value)}/>
 
       <h2>Filter Result</h2>
+      {!filter
+        ? <i>no result</i>
+        : (<ul>
+          {filteredUsers.map(user => <li key={user.id}>{user.username} - {user.contact}</li>)}
+        </ul>)
+      }
     </div>
   )
 }
