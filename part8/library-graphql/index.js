db3d368de1adbf110d2bdc47df981db786b136ad
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-10-29 11:14:32 +0100

    Modify the allBooks query with a different parameter

--- part8/library-graphql/index.js
+++ part8/library-graphql/index.js
@@ -116,7 +116,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String!): [Book!]!
+    allBooks(genre: String!): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -126,11 +126,13 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if(!args.author){
+      if(args.author){
+        return books.filter(book => book.author === args.author)
+      }else if(args.genre){
+        return books.filter(book => book.genres.includes(args.genre))
+      }else{
         return books
       }
-
-      return books.filter(book => book.author === args.author)
     },
     allAuthors: () => authors
   },
