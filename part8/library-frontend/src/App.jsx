d41526033360dd55b183841b51f6dcad2415c5d6
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-11-05 17:38:24 +0100

    Show all books

--- part8/library-frontend/src/App.jsx
+++ part8/library-frontend/src/App.jsx
@@ -3,13 +3,14 @@ import Authors from "./components/Authors";
 import Books from "./components/Books";
 import NewBook from "./components/NewBook";
 import { useQuery } from "@apollo/client/react";
-import { ALL_AUTHORS } from './queries'
+import { ALL_AUTHORS, ALL_BOOKS } from './queries'
 
 const App = () => {
   const [page, setPage] = useState("authors");
   const userResult = useQuery(ALL_AUTHORS);
+  const bookResult = useQuery(ALL_BOOKS)
 
-  if (userResult.loading) {
+  if (userResult.loading || bookResult.loading) {
     return <div>loading...</div>
   }
 
@@ -23,7 +24,7 @@ const App = () => {
 
       <Authors show={page === "authors"} authors={userResult.data.allAuthors}/>
 
-      <Books show={page === "books"} />
+      <Books show={page === "books"} books={bookResult.data.allBooks}/>
 
       <NewBook show={page === "add"} />
     </div>
