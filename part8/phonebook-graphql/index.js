e196f2494b0a16beb1654f98079120aff3310635
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-11-11 22:56:20 +0100

    Add parameter context into startStandaloneServer

--- part8/phonebook-graphql/index.js
+++ part8/phonebook-graphql/index.js
@@ -91,6 +91,7 @@ const resolvers = {
       return Person.find({ phone: { $exists: args.phone === 'YES' } })
     },
     findPerson: async (root, args) => Person.findOne({ name: args.name }),
+    me: (root, args, context) => { return context.currentUser}
   },
   Person: {
     address: (root) => {
@@ -175,6 +176,17 @@ const server = new ApolloServer({
 
 startStandaloneServer(server, {
   listen: { port: 4000 },
+  context: async ({ req, res }) => {
+    const auth = req ? req.headers.authorization : null
+    if (auth && auth.startsWith('Bearer ')) {
+      const decodedToken = jwt.verify(
+        auth.substring(7), process.env.JWT_SECRET
+      )
+      const currentUser = await User
+        .findById(decodedToken.id).populate('friends')
+      return { currentUser }
+    }
+  },
 }).then(({ url }) => {
   console.log(`Server ready at ${url}`)
 })
