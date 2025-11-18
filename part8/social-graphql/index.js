4517f82bd4acd43e2addec7fb04b14c4bbdee237
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-11-18 21:07:51 +0100

    Add enum

--- part8/social-graphql/index.js
+++ part8/social-graphql/index.js
@@ -39,9 +39,14 @@ const typeDefs = `
     id: ID!
   }
 
+  enum YesNo {
+    YES
+    NO
+  }
+
   type Query {
     personCount: Int!
-    allPersons: [Person!]!
+    allPersons(phone: YesNo): [Person!]!
     findPerson(name: String!): Person
   }
 
@@ -70,7 +75,14 @@ const resolvers = {
 
   Query: {
     personCount: () => persons.length,
-    allPersons: () => persons,
+    allPersons: (root, args) => {
+      if (!args.phone) {
+        return persons
+      }
+      const byPhone = (person) =>
+        args.phone === 'YES' ? person.phone : !person.phone
+      return persons.filter(byPhone)
+    },
     findPerson: (root, args) =>
       persons.find(p => p.name === args.name)
   },
