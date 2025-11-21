ccc16482c9b5cd14c744c9fc49ba0346dbb395c6
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-11-21 15:06:17 +0100

    Make client accessible for all components

--- part8/social-client/src/main.jsx
+++ part8/social-client/src/main.jsx
@@ -1,30 +1,15 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-import { ApolloClient, InMemoryCache, HttpLink, gql } from '@apollo/client'
+import { ApolloClient, InMemoryCache, HttpLink, ApolloProvider } from '@apollo/client'
 
 const client = new ApolloClient({
   link: new HttpLink({ uri: 'http://localhost:4000' }),
   cache: new InMemoryCache(),
 })
 
-const query = gql`
-  query {
-    allPersons  {
-      name,
-      phone,
-      address {
-        street,
-        city
-      }
-      id
-    }
-  }
-`
-
-client.query({ query })
-  .then((response) => {
-    console.log(response.data)
-  })
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+)
