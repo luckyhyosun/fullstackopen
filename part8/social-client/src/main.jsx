8c12634341361c0a03cae149206a54dbf55bb6ba
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-11-21 15:03:13 +0100

    Create a new client object with apollo/client

--- part8/social-client/src/main.jsx
+++ part8/social-client/src/main.jsx
@@ -1,4 +1,30 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+import { ApolloClient, InMemoryCache, HttpLink, gql } from '@apollo/client'
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: 'http://localhost:4000' }),
+  cache: new InMemoryCache(),
+})
+
+const query = gql`
+  query {
+    allPersons  {
+      name,
+      phone,
+      address {
+        street,
+        city
+      }
+      id
+    }
+  }
+`
+
+client.query({ query })
+  .then((response) => {
+    console.log(response.data)
+  })
+
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
