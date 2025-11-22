9bfeb7588fce19b09f4520550201260915172e48
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-11-22 22:59:25 +0100

    Use apollo/client and useQuery

--- part8/social-client/src/App.jsx
+++ part8/social-client/src/App.jsx
@@ -1,8 +1,27 @@
+import { gql } from "@apollo/client";
+import { useQuery } from "@apollo/client/react";
+
+const ALL_PERSONS = gql`
+query {
+  allPersons {
+    name
+    phone
+    id
+  }
+}
+`
+
 function App() {
+  const result = useQuery(ALL_PERSONS)
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+
   return (
-    <>
-      Client
-    </>
+    <div>
+      {result.data.allPersons.map(p => p.name).join(', ')}
+    </div>
   )
 }
 
--- part8/social-client/src/main.jsx
+++ part8/social-client/src/main.jsx
@@ -1,7 +1,8 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-import { ApolloClient, InMemoryCache, HttpLink, ApolloProvider } from '@apollo/client'
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client'
+import { ApolloProvider } from "@apollo/client/react";
 
 const client = new ApolloClient({
   link: new HttpLink({ uri: 'http://localhost:4000' }),
