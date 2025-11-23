4469105f110224be6fbaeaeeb668050f557bcd18
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-11-23 15:40:55 +0100

    Set named queries and variables

--- part8/social-client/src/App.jsx
+++ part8/social-client/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { gql } from "@apollo/client";
 import { useQuery } from "@apollo/client/react";
 
@@ -11,13 +12,58 @@ query {
 }
 `
 
+const FIND_PERSON = gql`
+  query findPersonByName($nameToSearch: String!) {
+    findPerson(name: $nameToSearch) {
+      name
+      phone
+      id
+      address {
+        street
+        city
+      }
+    }
+  }
+`
+
+function Person({person, onClose}) {
+  return (
+    <div>
+      <h2>{person.name}</h2>
+      <div>
+        {person.address.street} {person.address.city}
+      </div>
+      <div>{person.phone}</div>
+      <button onClick={onClose}>close</button>
+    </div>
+  )
+}
+
 function Persons({persons}) {
+  const [nameToSearch, setNameToSearch] = useState(null)
+  const result = useQuery(FIND_PERSON, {
+    variables: { nameToSearch },
+    skip: !nameToSearch,
+  })
+
+  if (nameToSearch && result.data) {
+    return (
+      <Person
+        person={result.data.findPerson}
+        onClose={() => setNameToSearch(null)}
+      />
+    )
+  }
+
   return(
     <div>
       <h2>Persons</h2>
       {persons.map(p =>
         <div key={p.name}>
           {p.name} - {p.phone}
+          <button onClick={() => setNameToSearch(p.name)}>
+            show address
+          </button>
         </div>
       )}
     </div>
