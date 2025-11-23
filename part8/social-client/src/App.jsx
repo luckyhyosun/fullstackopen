062e0760984de7efeea5492216f110e7faf4608e
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-11-23 15:29:53 +0100

    Add Persons component

--- part8/social-client/src/App.jsx
+++ part8/social-client/src/App.jsx
@@ -11,6 +11,19 @@ query {
 }
 `
 
+function Persons({persons}) {
+  return(
+    <div>
+      <h2>Persons</h2>
+      {persons.map(p =>
+        <div key={p.name}>
+          {p.name} - {p.phone}
+        </div>
+      )}
+    </div>
+  )
+}
+
 function App() {
   const result = useQuery(ALL_PERSONS)
 
@@ -20,7 +33,7 @@ function App() {
 
   return (
     <div>
-      {result.data.allPersons.map(p => p.name).join(', ')}
+      <Persons persons = {result.data.allPersons}/>
     </div>
   )
 }
