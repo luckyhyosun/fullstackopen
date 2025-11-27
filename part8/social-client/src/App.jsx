7aae0f8af2e61fa1504a82505aaeda48287ea2e9
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-11-27 23:39:16 +0100

    Add mutation error

--- part8/social-client/src/App.jsx
+++ part8/social-client/src/App.jsx
@@ -46,21 +46,42 @@ function Persons({persons}) {
       )}
 
       <h3>Add Person</h3>
-      <PersonForm />
+    </div>
+  )
+}
+
+const Notify = ({errorMessage}) => {
+  if ( !errorMessage ) {
+    return null
+  }
+  return (
+    <div style={{color: 'red'}}>
+    {errorMessage}
     </div>
   )
 }
 
 function App() {
+  const [errorMessage, setErrorMessage] = useState(null)
+
   const result = useQuery(ALL_PERSONS)
 
   if (result.loading) {
     return <div>loading...</div>
   }
 
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
+
   return (
     <div>
+      <Notify errorMessage={errorMessage} />
       <Persons persons = {result.data.allPersons}/>
+      <PersonForm setError={notify}/>
     </div>
   )
 }
