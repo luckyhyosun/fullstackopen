c87dd2540ed267ebc84dc796eefa7767abfd9bde
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-11-30 23:16:26 +0100

    Add validation on server side

--- part8/social-graphql/index.js
+++ part8/social-graphql/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const { GraphQLError } = require('graphql')
 
 require('dotenv').config()
 const mongoose = require('mongoose')
@@ -84,13 +85,35 @@ const resolvers = {
   Mutation: {
     addPerson: async (root, args) => {
       const person = new Person({...args})
-      return person.save()
+
+      try {
+        await person.save()
+      } catch (error) {
+        throw new GraphQLError('Saving person failed', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: args.name,
+            error
+          }
+        })
+      }
     },
 
     editNumber: async (root, args) => {
       const person = await Person.findOne({name: args.name})
       person.phone = args.phone
-      return person.save()
+
+      try {
+        await person.save()
+      } catch (error) {
+        throw new GraphQLError('Saving number failed', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: args.name,
+            error
+          }
+        })
+      }
     }
   }
 }
