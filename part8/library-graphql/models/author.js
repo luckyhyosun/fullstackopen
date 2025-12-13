6616ce902a8cdf43134d716a80e98e1397d49f25
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-12-13 14:52:04 +0100

    Add jwt and deactive uniqueValidator

--- part8/library-graphql/models/author.js
+++ part8/library-graphql/models/author.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const uniqueValidator = require('mongoose-unique-validator')
+// const uniqueValidator = require('mongoose-unique-validator')
 
 const schema = new mongoose.Schema({
   name: {
@@ -14,6 +14,6 @@ const schema = new mongoose.Schema({
   }
 })
 
-schema.plugin(uniqueValidator)
+// schema.plugin(uniqueValidator)
 
 module.exports = mongoose.model('Author', schema)
