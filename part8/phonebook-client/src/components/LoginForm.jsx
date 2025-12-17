1489497a7e5c414d0a3532f4607ff69787196ba2
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-12-17 16:26:29 +0100

    Add token login to LoginForm

--- part8/phonebook-client/src/components/LoginForm.jsx
+++ part8/phonebook-client/src/components/LoginForm.jsx
@@ -1,6 +1,51 @@
-const LoginForm = () => {
+import { useState, useEffect } from 'react'
+import { useMutation } from "@apollo/client/react";
+import { LOGIN } from '../queries'
+
+const LoginForm = ({ setError, setToken }) => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [login, result] = useMutation(LOGIN, {
+    onError: (error) => {
+      setError(error.graphQLErrors[0].message)
+    }
+  })
+
+  useEffect(() => {
+    if(result.data){
+      const token = result.data.login.value
+      setToken(token)
+      localStorage.setItem('phonenumbers-user-token', token)
+    }
+  }, [result.data])
+
+  const submit = async (event) => {
+    event.preventDefault()
+
+    login({ variables: { username, password } })
+  }
+
   return (
-    <div>Login</div>
+    <div>
+      <form onSubmit={submit}>
+        <div>
+          username <input
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+
+        <div>
+          passowrd <input
+            type='password'
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type='submit'>login</button>
+      </form>
+    </div>
   )
 }
 
