0856289fa603152cc713d2c76075eb01fd74694e
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-12-19 23:22:32 +0100

    Add validation

--- part8/phonebook-client/src/components/PersonForm.jsx
+++ part8/phonebook-client/src/components/PersonForm.jsx
@@ -23,7 +23,12 @@ const PersonForm = ({ setError }) => {
   const submit = (event) => {
     event.preventDefault()
 
-    createPerson({  variables: { name, phone, street, city } })
+    createPerson({
+      variables: {
+        name, street, city,
+        phone: phone.length > 0 ? phone : undefined
+      }
+    })
 
     setName('')
     setPhone('')
