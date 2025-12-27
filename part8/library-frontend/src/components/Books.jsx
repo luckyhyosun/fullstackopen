da7edd6c478eca69adcd9b07b76368a1c569be29
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2025-12-27 10:37:48 +0200

    Filter the book list by genre

--- part8/library-frontend/src/components/Books.jsx
+++ part8/library-frontend/src/components/Books.jsx
@@ -1,9 +1,19 @@
+import { useState } from 'react'
+
 const Books = (props) => {
   if (!props.show) {
     return null
   }
 
   const books = props.books || []
+  const [selectedGenre, setSelectedGenre] = useState('all')
+
+  const allGenres = Array.from(new Set(books.flatMap((b) => b.genres || [])))
+
+  const displayedBooks =
+    selectedGenre === 'all'
+      ? books
+      : books.filter((b) => (b.genres || []).includes(selectedGenre))
 
   return (
     <div>
@@ -17,16 +27,35 @@ const Books = (props) => {
             <th>published</th>
             <th>genres</th>
           </tr>
-          {books.map((a) => (
+          {displayedBooks.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
               <td>{a.published}</td>
-              <td>{a.genres.join(' / ')}</td>
+              <td>{(a.genres || []).join(' / ')}</td>
             </tr>
           ))}
         </tbody>
       </table>
+
+      <div style={{ marginTop: 16 }}>
+        <button
+          onClick={() => setSelectedGenre('all')}
+          disabled={selectedGenre === 'all'}
+        >
+          all
+        </button>
+        {allGenres.map((g) => (
+          <button
+            key={g}
+            onClick={() => setSelectedGenre(g)}
+            disabled={selectedGenre === g}
+            style={{ marginLeft: 8 }}
+          >
+            {g}
+          </button>
+        ))}
+      </div>
     </div>
   )
 }
