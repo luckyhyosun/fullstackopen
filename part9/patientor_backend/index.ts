f792d74bd122f7230cbf4d4633610b49512aab3a
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2026-01-13 13:41:00 +0100

    Install eslint and create eslint.config

--- part9/patientor_backend/index.ts
+++ part9/patientor_backend/index.ts
@@ -4,10 +4,10 @@ app.use(express.json());
 
 const PORT = '3003';
 
-app.get('/ping', (req, res) => {
-  res.send('pong')
-})
+app.get('/ping', (_req, res) => {
+  res.send('pong');
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-})
+});
