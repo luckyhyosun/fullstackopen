961b96a4f5e65fa353b104263517a6fec1fb9835
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2026-01-18 16:57:27 +0100

    Export data directly from a TypeScript file

--- part9/ilaryDiary/src/services/diaryService.ts
+++ part9/ilaryDiary/src/services/diaryService.ts
@@ -1,8 +1,6 @@
-import diaryData from '../data/entries.json';
+import diaries from '../data/entries';
 
 import { DiaryEntry } from '../types';
-
-const diaries: DiaryEntry[] = diaryData as DiaryEntry[];
 
 const getEntries = ():DiaryEntry[] => {
   return diaries;
