e462d05e595df7a6c348e0c56a708cb1d1770850
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2026-01-23 19:51:33 +0100

    Fix frotend port number and link to backend api

--- part9/patientor_frontend/vite.config.ts
+++ part9/patientor_frontend/vite.config.ts
@@ -5,10 +5,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server:{
+    port: 3000,
     proxy: {
       //connect to backend
-      '/ping': {
-        target: 'http://localhost:3001',
+      '/': {
+        target: 'http://localhost:3001/api/patients',
         changeOrigin: true
       }
     }
