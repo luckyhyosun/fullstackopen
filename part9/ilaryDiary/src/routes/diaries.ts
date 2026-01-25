5832bf6aea9d1ddb8ab811a97b6ffc8b54211a86
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2026-01-25 20:19:21 +0100

    Preventing an accidental undefined result

--- part9/ilaryDiary/src/routes/diaries.ts
+++ part9/ilaryDiary/src/routes/diaries.ts
@@ -1,10 +1,20 @@
 import express from 'express';
-import getNonSensitiveEntries from '../services/diaryService';
+import diaryService from '../services/diaryService';
 
 const router = express.Router();
 
 router.get('/', (_req, res) => {
-  res.send(getNonSensitiveEntries.getNonSensitiveEntries());
+  res.send(diaryService.getNonSensitiveEntries());
+});
+
+router.get('/:id', (req, res) => {
+  const diary = diaryService.findById(Number(req.params.id));
+
+  if (diary) {
+    res.send(diary);
+  } else {
+    res.sendStatus(404);
+  }
 });
 
 router.post('/', (_req, res) => {
