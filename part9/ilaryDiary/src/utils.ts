2ccb047c9d89686a8f5e5b536902553b7b8336b5
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2026-01-28 17:55:54 +0100

    Validating requests

---
+++ part9/ilaryDiary/src/utils.ts
@@ -0,0 +1,14 @@
+import { NewDiaryEntry } from './types';
+
+const toNewDiaryEntry = (object: unknown):NewDiaryEntry => {
+  const newEntry: NewDiaryEntry = {
+    weather: 'cloudy', // fake the return value
+    visibility: 'great',
+    date: '2022-1-1',
+    comment: 'fake news'
+  };
+
+  return newEntry;
+};
+
+export default toNewDiaryEntry;
