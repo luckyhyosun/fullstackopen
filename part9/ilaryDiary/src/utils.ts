778a3c5cd6abcd70e9e2077dc83b186cfc1f9697
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2026-01-29 19:48:16 +0100

    Create type guard

--- part9/ilaryDiary/src/utils.ts
+++ part9/ilaryDiary/src/utils.ts
@@ -1,11 +1,23 @@
 import { NewDiaryEntry } from './types';
 
+const isString = (text: unknown): text is string => {
+  return typeof text === 'string' || text instanceof String;
+};
+
+const parseComment = (comment: unknown): string => {
+  if (!comment || !isString(comment)) {
+    throw new Error('Incorrect or missing comment');
+  }
+
+  return comment;
+};
+
 const toNewDiaryEntry = (object: unknown):NewDiaryEntry => {
   const newEntry: NewDiaryEntry = {
     weather: 'cloudy', // fake the return value
     visibility: 'great',
     date: '2022-1-1',
-    comment: 'fake news'
+    comment: parseComment(object.comment)
   };
 
   return newEntry;
