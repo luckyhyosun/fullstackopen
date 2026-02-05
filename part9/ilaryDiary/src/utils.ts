eb39630413c5d864867f35e9ed34914bc6380f32
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2026-02-05 18:02:34 +0100

    Use schema validator's Enum

--- part9/ilaryDiary/src/utils.ts
+++ part9/ilaryDiary/src/utils.ts
@@ -1,43 +1,6 @@
 import { NewDiaryEntry, Visibility, Weather } from './types';
 import { z } from 'zod';
 
-const isVisibility = (param: string): param is Visibility => {
-  return Object.values(Visibility).map(v => v.toString()).includes(param);
-};
-
-const parseVisibility = (visibility: unknown): Visibility => {
-  if (!isString(visibility) || !isVisibility(visibility)) {
-      throw new Error('Incorrect or missing visibility: ' + visibility);
-  }
-  return visibility;
-};
-
-const isWeather = (param: string): param is Weather => {
-  return Object.values(Weather).map(v => v.toString()).includes(param);
-};
-
-const parseWeather = (weather: unknown): Weather => {
-  if (!weather || !isString(weather) || !isWeather(weather)) {
-      throw new Error('Incorrect or missing weather: ' + weather);
-  }
-  return weather;
-};
-
-const isDate = (date: string): boolean => {
-  return Boolean(Date.parse(date));
-};
-
-const parseDate  = (date: unknown): string => {
-  if (!date || !isString(date) || !isDate(date)) {
-      throw new Error('Incorrect or missing date: ' + date);
-  }
-  return date;
-};
-
-const isString = (text: unknown): text is string => {
-  return typeof text === 'string' || text instanceof String;
-};
-
 const toNewDiaryEntry = (object: unknown): NewDiaryEntry => {
   if ( !object || typeof object !== 'object' ) {
     throw new Error('Incorrect or missing data');
@@ -45,9 +8,9 @@ const toNewDiaryEntry = (object: unknown): NewDiaryEntry => {
 
   if ('comment' in object && 'date' in object && 'weather' in object && 'visibility' in object)  {
     const newEntry: NewDiaryEntry = {
-      weather: parseWeather(object.weather),
-      visibility: parseVisibility(object.visibility),
-      date: parseDate(object.date),
+      weather: z.nativeEnum(Weather).parse(object.weather),
+      visibility: z.nativeEnum(Visibility).parse(object.visibility),
+      date: z.string().date().parse(object.date),
       comment: z.string().parse(object.comment)
     };
 
