950105ccabc90521b8c1c22867869fc04d8e2ef0
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2026-02-05 20:00:43 +0100

    Make 'comment' optional and use 'infer' to type

--- part9/ilaryDiary/src/utils.ts
+++ part9/ilaryDiary/src/utils.ts
@@ -1,12 +1,12 @@
 import { NewDiaryEntry, Visibility, Weather } from './types';
 import { z } from 'zod';
 
-const newEntrySchema = z.object({
+export const newEntrySchema = z.object({
   weather: z.nativeEnum(Weather),
   visibility: z.nativeEnum(Visibility),
   date: z.string().date(),
-  // comment: z.string().optional()
-  comment: z.string()
+  comment: z.string().optional()
+  // comment: z.string()
 });
 
 export const toNewDiaryEntry = (object: unknown): NewDiaryEntry => {
