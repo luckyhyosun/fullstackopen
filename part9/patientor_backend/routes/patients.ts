a2591d8302fc0e2fe2617770161a762457bd68c4
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2026-02-10 18:58:23 +0100

    Add Error handling middleware

--- part9/patientor_backend/routes/patients.ts
+++ part9/patientor_backend/routes/patients.ts
@@ -2,6 +2,7 @@ import express, { Request, Response, NextFunction } from 'express';
 import patientsService from '../services/patientsService';
 import newPatientSchema from '../utils';
 import { newPatientEntry, Patient } from '../types';
+import { z } from 'zod';
 
 const router = express.Router();
 
@@ -18,9 +19,19 @@ const newPatientParser = (req: Request, _res: Response, next: NextFunction) => {
   }
 };
 
+const errorMiddleware = (error: unknown, _req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof z.ZodError) {
+    res.status(400).send({ error: error.issues });
+  } else {
+    next(error);
+  }
+};
+
 router.post('/', newPatientParser, (req: Request<unknown, unknown, newPatientEntry>, res: Response<Patient>) => {
   const addedPatient = patientsService.addPatient(req.body);
   res.json(addedPatient);
 });
+
+router.use(errorMiddleware);
 
 export default router;
