1f75251ca94f409d05d241e2c174e4e91dac7a3e
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2026-02-20 21:33:46 +0100

    Clean the previous codes and set the note useState

--- part9/reactTypescript/src/main.tsx
+++ part9/reactTypescript/src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client'
-import { Welcome } from './Welcome';
+import App  from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Welcome name="Sarah" />
+  <App />
 )
