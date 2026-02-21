3db33062857e57130ce2a88ce7493ab9ac551dd7
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2026-02-21 09:56:09 +0100

    React app with state

--- part9/reactTypescript/src/App.tsx
+++ part9/reactTypescript/src/App.tsx
@@ -11,9 +11,26 @@ const App = () => {
   ]);
   const [newNote, setNewNote] = useState('');
 
-  return (
+  const noteCreation = (event: React.SyntheticEvent) => {
+    event.preventDefault()
+    const noteToAdd = {
+      content: newNote,
+      id: String(notes.length + 1)
+    }
+    setNotes(notes.concat(noteToAdd));
+    setNewNote('')
+  };
 
+  return (
     <div>
+      <form onSubmit={noteCreation}>
+        <input
+         type="text"
+          value={newNote}
+          onChange={(event) => setNewNote(event.target.value)}
+        />
+        <button type="submit">add</button>
+      </form>
       <ul>
         {notes.map(note =>
           <li key={note.id}>{note.content}</li>
