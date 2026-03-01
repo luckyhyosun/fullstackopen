05b50a311d45ad84b0e88f991cfa170f1d43a75f
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2026-03-01 20:31:09 +0100

    Install axios and connect backedn to frontend

--- part9/ilaryDiary_frontend/src/App.tsx
+++ part9/ilaryDiary_frontend/src/App.tsx
@@ -1,4 +1,14 @@
+import { useEffect } from "react";
+import axios from "axios";
+
+
 function App() {
+  useEffect(() => {
+    axios.get('/ping').then(response => {
+      console.log(response.data);
+    })
+  }, [])
+
   return (
     <>
       <p>hello</p>
