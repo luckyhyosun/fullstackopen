ae081769fb200366531ccf9699a1e01b032231ec
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2026-03-11 19:44:52 +0100

    Set useStat to each input

--- part9/ilaryDiary_frontend/src/components/NewEntry.tsx
+++ part9/ilaryDiary_frontend/src/components/NewEntry.tsx
@@ -1,4 +1,11 @@
+import { useState } from 'react';
+
 const NewEntry = () => {
+  const [date, setDate] = useState('');
+  const [visibility, setVisibility] = useState('');
+  const [weather, setWeather] = useState('');
+  const [comment, setComment] = useState('');
+
   const addDiary = (event:React.SyntheticEvent) => {
     event.preventDefault();
   }
@@ -6,10 +13,29 @@ const NewEntry = () => {
   return (
     <>
       <form onSubmit={addDiary}>
-        date: <input type="text"></input><br />
-        visibility: <input type="text"></input><br />
-        weather: <input type="text"></input><br />
-        comment: <input type="text"></input><br />
+        date: <input
+        type="text"
+        value={date}
+        onChange={({target}) => setDate(target.value)}
+        ></input><br />
+
+        visibility: <input
+        type="text"
+        value={visibility}
+        onChange={({target}) => setVisibility(target.value)}
+        ></input><br />
+
+        weather: <input
+        type="text"
+        value={weather}
+        onChange={({target}) => setWeather(target.value)}
+        ></input><br />
+
+        comment: <input
+        type="text"
+        value={comment}
+        onChange={({target}) => setComment(target.value)}
+        ></input><br />
         <button type="submit">Add</button>
       </form>
     </>
