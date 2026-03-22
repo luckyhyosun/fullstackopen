e7160f42b150a82738185970ee75d0a87f8fca04
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2026-03-22 21:01:56 +0100

    Modify the input form with date and radio input

--- part9/ilaryDiary_frontend/src/components/NewEntry.tsx
+++ part9/ilaryDiary_frontend/src/components/NewEntry.tsx
@@ -22,26 +22,44 @@ const NewEntry = ({ onSubmit }: Props) => {
     })
   }
 
+  const onchangeVisibility = (e) => {
+    setVisibility(e.target.value);
+  }
+
+  const onChangeWeather = (e) => {
+    setWeather(e.target.value);
+  }
+
   return (
     <>
       <form onSubmit={addDiary}>
         date: <input
-        type="text"
+        type="date"
         value={date}
         onChange={({target}) => setDate(target.value)}
         ></input><br />
 
-        visibility: <input
-        type="text"
-        value={visibility}
-        onChange={({target}) => setVisibility(target.value)}
-        ></input><br />
+        visibility:
+        <input type="radio" id="great" value="great" name="visibility" onChange={onchangeVisibility} />
+        <label htmlFor="great">great</label>
+        <input type="radio" id="good" value="good" name="visibility" onChange={onchangeVisibility} />
+        <label htmlFor="good">good</label>
+        <input type="radio" id="ok" value="ok" name="visibility" onChange={onchangeVisibility} />
+        <label htmlFor="ok">ok</label>
+        <input type="radio" id="poor" value="poor" name="visibility" onChange={onchangeVisibility} />
+        <label htmlFor="poor">poor</label><br />
 
-        weather: <input
-        type="text"
-        value={weather}
-        onChange={({target}) => setWeather(target.value)}
-        ></input><br />
+        weather:
+        <input type="radio" id="sunny" value="sunny" name="weather" onChange={onChangeWeather} />
+        <label htmlFor="great">sunny</label>
+        <input type="radio" id="rainy" value="rainy" name="weather" onChange={onChangeWeather} />
+        <label htmlFor="good">rainy</label>
+        <input type="radio" id="cloudy" value="cloudy" name="weather" onChange={onChangeWeather} />
+        <label htmlFor="ok">cloudy</label>
+        <input type="radio" id="stormy" value="stormy" name="weather" onChange={onChangeWeather} />
+        <label htmlFor="poor">stormy</label>
+        <input type="radio" id="windy" value="windy" name="weather" onChange={onChangeWeather} />
+        <label htmlFor="poor">windy</label><br />
 
         comment: <input
         type="text"
