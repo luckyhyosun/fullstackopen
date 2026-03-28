88c9a151d6eeea91acf027a732d33404cc592545
Lucky Hyosun <hellobonjour.lucky@gmail.com>
2026-03-28 21:17:13 +0100

    Reverse type

--- part9/patientor_backend/types.ts
+++ part9/patientor_backend/types.ts
@@ -13,10 +13,6 @@ export interface Diagnosis {
   latin?: string;
 }
 
-export interface Entry {
-
-}
-
 export interface Patient {
   id: string;
   name: string;
@@ -24,7 +20,6 @@ export interface Patient {
   ssn: string;
   gender: string;
   occupation: string;
-  entries: Entry[]
 }
 
 export type PatientEntry = Omit<Patient, 'ssn' | 'entries'>;
